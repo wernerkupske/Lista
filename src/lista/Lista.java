21b7a5eb361965c21c35ac6d4c22b795f2ecd0a7
korac <informatica@korac.blumenau.ifc.edu.br>
2017-11-10 09:17:36 -0200

    Fazer lista

--- src/lista/Lista.java
+++ src/lista/Lista.java
@@ -14,8 +14,26 @@ public class Lista {
     /**
      * @param args the command line arguments
      */
+    //termino.proximo = novo;
+    //termino = novo;
+    public void adiciona(){
+        
+    }
+    
+    //if(inicio != null){
+    //  inicio = inicio.proximo;
+    //}
+    public void remove(){
+        Lista lista = new Lista();
+        
+    }
+    
+    public void lista(){
+        
+    }
+    
     public static void main(String[] args) {
-        // TODO code application logic here
+        
     }
     
 }
